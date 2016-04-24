bfa3af855c332d8824e4099318237f7b7d749cf4
philippelecoq <mypidrone@gmail.com>
2016-04-24 17:32:47 +0000

    v1.5 Sun 24 Apr 17:32:37 UTC 2016

--- build.sh
+++ build.sh
@@ -11,13 +11,17 @@ python setup.py build install
 [ $? -ne 0 ] && exit 1
 cp MAVProxy/modules/mavproxy_MyPiModule.py MyPiModule2/
 cp /usr/local/bin/StartArduCopter-quad.sh MyPiModule2/
+cp /lib/systemd/system/ArduCopter-quad.service MyPiModule2/
+cp /lib/systemd/system/myvideo.service MyPiModule2/
+cp /lib/systemd/system/mavproxy.service MyPiModule2/
+cp /usr/local/bin/start_video.sh MyPiModule2/
 cp /root/fpv/TESTS/start_tx_with_video_recording_broadcast_over_ap.sh MyPiModule2/
 cp /root/fpv/start_tx_with_video_recording.sh MyPiModule2/
 cp /etc/rc.local MyPiModule2/
 cd MyPiModule2
 VERSION=`grep "self.myversion" mavproxy_MyPiModule.py|head -n 1|awk -F'"' '{print "v"$2}'`
 echo "mavproxy_MyPiModule.py VERSION=$VERSION"
-LIST="mav.parm build.sh mavproxy_MyPiModule.py StartArduCopter-quad.sh start_tx_with_video_recording.sh start_tx_with_video_recording_broadcast_over_ap.sh start_rx.sh start_ap.sh start_video.sh rc.local README.md"
+LIST="mav.parm build.sh mavproxy_MyPiModule.py StartArduCopter-quad.sh start_tx_with_video_recording.sh start_tx_with_video_recording_broadcast_over_ap.sh start_rx.sh start_ap.sh start_video.sh rc.local ArduCopter-quad.service myvideo.service mavproxy.service README.md"
 git add $LIST
 git commit $LIST -m "$VERSION $date"
 #git commit StartArduCopter-quad.sh -m "$date"
@@ -28,7 +32,7 @@ git commit $LIST -m "$VERSION $date"
 #git commit rc.local -m "$date"
 #git commit build.sh -m "$date"
 #git commit README.md -m "$date"
-#git pull
+git pull
 git push
 cd $MYDIR
 C=`ps -ef |grep -v grep |grep -c /usr/local/bin/mavproxy.py`
