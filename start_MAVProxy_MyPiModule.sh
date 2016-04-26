9f3d0b8cfecf7a12cb407b344316398cac4657e4
philippelecoq <mypidrone@gmail.com>
2016-04-26 19:00:37 +0000

    v1.5 Tue 26 Apr 19:00:25 UTC 2016

--- start_MAVProxy_MyPiModule.sh
+++ start_MAVProxy_MyPiModule.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "MAVProxy is started and MyPiModule,mode modules loaded."
 echo "Log here /var/log/ArduCopter-quad-mavproxy.log"
-nohup /usr/bin/python /root/MyPiDrone2/MAVProxy-1.4.43/MAVProxy/mavproxy.py --master=udp:127.0.0.1:14550 --quadcopter --out=/dev/ttyUSB0,57600 --daemon --show-errors --default-modules='MyPiModule,mode' 1>/var/log/ArduCopter-quad-mavproxy.log 2>&1 &
+nohup /usr/bin/python /usr/local/bin/mavproxy.py --master=udp:127.0.0.1:14550 --quadcopter --out=/dev/ttyUSB0,57600 --daemon --show-errors --default-modules='MyPiModule,mode' 1>/var/log/ArduCopter-quad-mavproxy.log 2>&1 &
 echo "MAVProxy started PID $!"
